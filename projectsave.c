afd4523a6aa9c550b92ce68ab06a3ecc2b09e8dc
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-09-22 11:34:03 +0900

    Update projectsave.c

--- projectsave.c
+++ projectsave.c
@@ -1,13 +1,14 @@
 #include <stdio.h> 
 #include <stdbool.h>
+#include <math.h>
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable:4996)
 void V2(int num);
-double fouranglehornV(double a,double b,double h);
-float V(float r,float h);
+double fouranglehornV(double a, double b, double h);
+float V(float r, float h);
 int is_prime(int);
 int get_integer(void);
-
+float Vwithmathh(float,float);
 int main() {
 	/*float result1;
 	int result2;
@@ -40,37 +41,60 @@ int main() {
 	else {
 		printf("점수를 다시 입력하세요.");
 	}*/
-	
+
 	//함수란?
 	/*사용자 함수를 만드는 방법?
-	* 
+	*
 	*/
 	/*
 	printf("%6f",fouranglehornV(10,10,10));
 	printf("%lf", V(3, 1));*/
 
-
+	/*
 	//소수의 합 구하기
-	int num,result;
+	int num, result;
 
 	num = get_integer();
 	int sum = 0;
 	float phi = num;
-	for (int i = 1; i < num+1; i++) {
+	for (int i = 1; i < num + 1; i++) {
 		result = is_prime(i);
 		if (result == 1) {
 			sum += i;
 			float x = i;
-			if (num % i==0) {
+			if (num % i == 0) {
 				phi = phi * (1 - 1 / x);
 			}
 		}
 	}
-	printf("오일러 수: %f",phi);
-	printf("소수의 합:%d", sum);
-	return 0; 
+	printf("오일러 수: %f", phi);
+	printf("소수의 합:%d", sum);*/
+
+	/*
+	//math.h 라이브러리 사용해보기
+	float pi = acos(-1);
+	double f1 = 0.12456;
+	printf("%g\n", ceil(f1)); // 올림함수
+	printf("%g\n", floor(f1)); //내림함수
+	printf("%d\n", abs((int)floor(-f1)));//x:int, maximum(x,0)+maximum(-x,0)
+	printf("%g\n", fabs(-f1));*/
+
+	//math library를 사용해 원뿔의 부피 구하기
+	float r, h;
+	r = 10;
+	h = 3.1;
+	printf("pi*%g*%g*%g/3=%f",r,r,h,Vwithmathh(r,h));
+	
+	return 0;
 }
-double fouranglehornV(double a, double b, double h){
+float Vwithmathh(float r,float h) {
+	r = r * r;
+	float pi = acos(-1);
+	return pi * r * h / 3;
+}
+
+
+double fouranglehornV(double a, double b, double h) {
 	return a * b * h / 3;
 }
 float V(float r, float h) {
@@ -82,7 +106,7 @@ void V2(int num) {
 	if (num >= 95) {
 		printf("%d점은 A+에 해당합니다.", num);
 	}
-	else if (num < 95 && num>=90) {
+	else if (num < 95 && num >= 90) {
 		printf("%d점은 A에 해당합니다.", num);
 	}
 	else if (num < 90 && num >= 85) {
@@ -103,22 +127,22 @@ void V2(int num) {
 	else if (num < 65 && num >= 60) {
 		printf("%d점은 C에 해당합니다.", num);
 	}
-	else if (num < 60 ) {
+	else if (num < 60) {
 		printf("%d점은 F에 해당합니다.", num);
 	}
 }
 int is_prime(int n) {
 	int divisors = 0, i;
-	for (i = 1;i<=n; i++) {
-		if ((n % i == 0) || (i%n==0)) {
+	for (i = 1; i <= n; i++) {
+		if ((n % i == 0) || (i % n == 0)) {
 			divisors++;
 		}
 	}
-	return (divisors == 2 && n!=1);
+	return (divisors == 2 && n != 1);
 }
 int get_integer() {
 	int n;
 	printf("정수를 입력하시오:");
-	scanf_s("%d",&n);
+	scanf_s("%d", &n);
 	return n;
 }
