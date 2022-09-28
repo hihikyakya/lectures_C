1a693b6590b6917e19380cc54002f351d3c108dc
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-09-28 15:37:14 +0900

    Update programsave2.c

--- programsave2.c
+++ programsave2.c
@@ -1,9 +1,11 @@
 #include <stdio.h> 
 #include <stdbool.h>
 #include <math.h>
+#include <cmath>
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable:4996)
 
+double round(double);
 int main() {
 	unsigned int speed; //?
 	speed = -10;
@@ -13,7 +15,7 @@ int main() {
 	
 	//int1 = 20 -> error;
 	//printf("%d", (int1));
-	
+	/*
 	int i;
 	float f,f0,f1,f2;
 	double d;
@@ -26,5 +28,20 @@ int main() {
 	f1 = 34567891;
 	f2 = 34567892;
 	printf("f0 = %f, f1 = %f, f2 = %f",f0,f1,f2);
+	*/
+	
+	//1.47이란 숫자를 주고 반올림하기
+	double ROUND,num=1.6;
+	ROUND=round(num);
+	printf("%f",ROUND);
 	return 0;
+}
+double round(double a){
+	int num=(int)a;
+	if ((a-num)>0.5){
+		return ceil(a);
+	}
+	else{
+		return floor(a);
+	}
 }
