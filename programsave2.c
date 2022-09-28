2f9c3ff7123936e7d83a58935198634add72a96d
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-09-28 14:54:44 +0900

    Update programsave2.c

--- programsave2.c
+++ programsave2.c
@@ -12,6 +12,19 @@ int main() {
 	const int int1=10;
 	
 	//int1 = 20 -> error;
-	printf("%d", (int1));
+	//printf("%d", (int1));
+	
+	int i;
+	float f,f0,f1,f2;
+	double d;
+	i=34567890;
+	f=34567890;
+	d=34567890;
+	
+	printf("i = %d, f = %f, d = %f\n",i,f,d);
+	f0 = 34567889;
+	f1 = 34567891;
+	f2 = 34567892;
+	printf("f0 = %f, f1 = %f, f2 = %f",f0,f1,f2);
 	return 0;
 }
