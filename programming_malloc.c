77deed6c30452cb63a239a8f6a44c94281073222
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-11-24 11:52:11 +0900

    Update programming_malloc.c

--- programming_malloc.c
+++ programming_malloc.c
@@ -22,6 +22,7 @@ int main(){
 		printf("%u",*(p+i));
 	*/
 	
+	/*
 	book *pb;
 	pb=(book *)malloc(2*sizeof(book));
 	if(pb==NULL){
@@ -32,5 +33,33 @@ int main(){
 	pb[1].number=2;
 	strcpy(pb[1].title,"Data Structure");
 	free(pb); //동적메모리가 할당된 포인터를 해제시켜줌. 
+	*/
+	
+	/*
+	int *p;
+	int a=30,b=20,c=10;
+	p=(int *)calloc(2,100*sizeof(int)); //다 0으로 초기화
+	p=(int *)realloc(p,12*sizeof(int)); //메모리 크기가 변경됨. 
+	*p=a;
+	*(p+1)=b;
+	*(p+2)=c;
+	
+	
+	for(int i=0;i<1000;i++)
+		printf("%u",*(p+i));
+	*/
+	
+	
+	int *p;
+	int a=30,b=20,c=10;
+	p=(int *)malloc(2*sizeof(int)); //다 0으로 초기화
+	*p=a;
+	*(p+1)=b;
+	p=(int *)realloc(p,3*sizeof(int)); //메모리 크기가 변경됨. 
+	*(p+2)=c;
+	for(int i=0; i< sizeof(*p)/sizeof(int);i++){
+		printf("%d ",*(p+i));	
+	}
+	
 	return 0;
 }
