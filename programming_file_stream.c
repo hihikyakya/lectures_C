e58d0cb4dacc5dce140e277e4ac36099c26cf3ee
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-12-01 11:43:19 +0900

    Create programming_file_stream.c

---
+++ programming_file_stream.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 3
+
+typedef struct student{
+	int number;
+	char name[20];
+	double gpa;
+}Student;
+
+int main(){
+	/*
+	Student table[SIZE]={
+		{1,"Kim",3.99},
+		{2,"Min",2.68},
+		{3,"Lee",4.01}
+	};
+	
+	Student s;
+	FILE *fp=NULL;
+	int i;
+	if ((fp=fopen("student.dat","wb"))==NULL){
+		fprintf(stderr,"출력을 위한 파일을 열수 없습니다. \n");
+		exit(1);
+	}
+	
+	//배열을 파일에 저장함
+	fwrite(table,sizeof(Student),SIZE,fp);
+	fclose(fp);
+	//이진 파일을 읽기 모드로 연다.
+	if ((fp=fopen("student.dat","wb"))==NULL){
+		fprintf(stderr,"출력을 위한 파일을 열수 없습니다. \n");
+		exit(1);
+	}
+	for(i=0;i<SIZE;i++){
+		fread(&s,sizeof(Student),1,fp); //한줄씩 파일에 입력된것을 찾는다. 
+		printf("학번=%d,이름=%s,평점=%lf\n",s.number,s.name,s.gpa);
+	}
+	fclose(fp);*/
+	
+	//파일내용을 다른 파일로 옮 
+	FILE *fp1,*fp2;
+	char file1[100],file2[100];
+	char buffer[1024];
+	int count;
+	printf("첫번째 파일 이름:");
+	scanf("%s",file1);
+	printf("두번째 파일 이름: ");
+	scanf("%s",file2);
+	if((fp1=fopen(file1,"rb"))==NULL){
+		fprintf(stderr,"출력을 위한 파일을 열수 없습니다. \n");
+		exit(1);
+	} 
+	if((fp2=fopen(file2,"rb"))==NULL){
+		fprintf(stderr,"출력을 위한 파일을 열수 없습니다. \n");
+		exit(1);
+	} 
+	while((count=fread(buffer,sizeof(char),1024,fp1)) >0){
+		fwrite(buffer,sizeof(char),count,fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	return 0;
+}
