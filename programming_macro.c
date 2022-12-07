22204345b8d97bd993f86c62a726c29dc2d0448d
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-12-07 14:54:45 +0900

    Create programming_macro.c

---
+++ programming_macro.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+#define USA
+#define DEBUG
+#ifndef PI
+#define PI 3.141592
+#endif
+#ifndef SQUARE
+#define SQUARE(r) (r)*(r)
+#endif
+double area(double radius){
+	double result=0.0;
+	#ifdef DEBUG
+	#ifdef USA
+	printf("area(%f) is called \n",radius);
+	#else
+	printf("area(%f)가 호출되었음",radius);
+	#endif
+	#endif
+	result=PI*SQUARE(radius);
+	return result;
+}
+
+int main() {
+	printf("%d",area(MAX_SIZE));
+	
+	
+	return 0;
+}
