f4d04a9527d3d1a1e518bc405eaa4d924a7d9dc9
hihi <82352946+hihikyakya@users.noreply.github.com>
2022-12-07 15:16:23 +0900

    Update programming_macro.c

--- programming_macro.c
+++ programming_macro.c
@@ -10,13 +10,13 @@
 #endif
 double area(double radius){
 	double result=0.0;
-	#ifdef DEBUG
-	#ifdef USA
+#ifdef DEBUG
+#ifdef USA
 	printf("area(%f) is called \n",radius);
-	#else
+#else
 	printf("area(%f)가 호출되었음",radius);
-	#endif
-	#endif
+#endif
+#endif
 	result=PI*SQUARE(radius);
 	return result;
 }
@@ -24,6 +24,6 @@ double area(double radius){
 int main() {
 	printf("%d",area(MAX_SIZE));
 	
-	
+	//집가서 이중 포인터로 행렬 만들어보기! 
 	return 0;
 }
